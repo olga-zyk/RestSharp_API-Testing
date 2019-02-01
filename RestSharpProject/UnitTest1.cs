46e0d760beb009499028b29defc05b2f29a4a328
Olga Zyk <my.agleh@gmail.com>
2019-02-01 22:52:14 +0200

    Add json deserializer

--- RestSharpProject/UnitTest1.cs
+++ RestSharpProject/UnitTest1.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serialization.Json;
+
 
 namespace RestSharpProject
 {
@@ -8,13 +14,37 @@ namespace RestSharpProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ListAllRobotModels()
         {
             var client = new RestClient("http://jsonapi-robot-wars.herokuapp.com/");
 
             var request = new RestRequest("robotModels/", Method.GET);
 
             var response = client.Execute(request);
+
+            // deserialize json to dictionary using RestSharp Deserializer (string)
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var output_data = output["data"];
+            Console.WriteLine(output["data"] + Environment.NewLine);
+            Console.WriteLine(output_data + Environment.NewLine);
+
+            //TODO: add exception
+
+            // using Json.NET Deserializer (object {Newtonsoft.Json.Linq.JObject})
+            var json_object = JsonConvert.DeserializeObject(response.Content);
+            Console.WriteLine(json_object + Environment.NewLine);
+
+            //TODO: add exception
+
+            // using Json.NET Deserializer (Newtonsoft.Json.Linq.JObject)
+            JObject json_data = JObject.Parse(response.Content);
+            Console.WriteLine(json_data);
+
+            //TODO: add exception
+
+
+            //TODO: finally add Assert
         }
     }
 }
